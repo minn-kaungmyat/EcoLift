679b67fc0e16dbe5388e48abb308e75555d51957
minn-kaungmyat <minnkaungmyat00@gmail.com>
2025-08-11 23:08:36 +0700

    update home page

--- EcoLift/wwwroot/js/site.js
+++ EcoLift/wwwroot/js/site.js
@@ -2,3 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// Navbar scroll effect
+document.addEventListener('DOMContentLoaded', function() {
+    const navbar = document.querySelector('.navbar');
+    
+    if (navbar) {
+        window.addEventListener('scroll', function() {
+            if (window.scrollY > 50) {
+                navbar.classList.add('scrolled');
+            } else {
+                navbar.classList.remove('scrolled');
+            }
+        });
+    }
+});
